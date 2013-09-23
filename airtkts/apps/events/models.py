b5e2f6a15ee0cfa90a08fd790bbfc1e0345dec60
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-09-23 14:28:43 -0400

    Added Support for Empty Descriptions

--- airtkts/apps/events/models.py
+++ airtkts/apps/events/models.py
@@ -6,7 +6,7 @@ class Event(models.Model):
     slug = models.SlugField()
     name = models.CharField(max_length=100)
     owner = models.ManyToManyField(User, related_name='owner')
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     location = models.CharField(max_length=100)
