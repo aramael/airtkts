5e4beb60854cc15a5094153d95690da9dceb030c
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-09-23 19:03:54 -0400

    Added Templates and Views for New Events

--- airtkts/apps/events/forms.py
+++ airtkts/apps/events/forms.py
@@ -5,6 +5,8 @@ from airtkts.libs.forms import FieldsetsForm, ActionMethodForm, HideSlugForm
 
 class EventForm(ActionMethodForm, HideSlugForm, FieldsetsForm, forms.ModelForm):
 
+    POSSIBLE_ACTIONS = {'_save', '_addanother', '_continue'}
+
     fieldsets = (
         (None, {
             'fields': ('name', 'description',)
@@ -19,3 +21,11 @@ class EventForm(ActionMethodForm, HideSlugForm, FieldsetsForm, forms.ModelForm):
 
     class Meta:
         model = Event
+
+    def location_redirect(self, action, instance):
+        if action == '_save':
+            return {"to": 'event_home'}
+        elif action == '_addanother':
+            return {"to": 'event_new'}
+        elif action == '_continue':
+            return {"to": 'event_edit', 'event_id': instance.pk, 'event_slug':instance.slug}
--- airtkts/views.py
+++ airtkts/views.py
@@ -3,7 +3,9 @@ Django views for airtkts project.
 
 """
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from airtkts.apps.events.forms import EventForm
+from airtkts.apps.events.models import Event
 
 
 def home(request):
@@ -12,3 +14,38 @@ def home(request):
     context = {}
 
     return render(request, '', context)
+
+
+def event_home(request):
+    """    Display the Landing Page    """
+
+    events = Event.objects.all()
+
+    context = {
+        'events': events,
+    }
+
+    return render(request, 'event_home.html', context)
+
+
+def event_form(request, event_id=None, event_slug=None):
+    """    Display the Landing Page    """
+
+    if event_id is not None:
+        event = get_object_or_404(Event, pk=event_id)
+        if event_slug != event.slug:
+            redirect('event_edit', event_id=event.pk, event_slug=event.slug)
+    else:
+        event = None
+
+    form = EventForm(instance=event, data=request.POST or None, files=request.FILES or None)
+
+    if form.is_valid():
+        location_redirect = form.save()
+        return redirect(**location_redirect)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'event_form.html', context)
