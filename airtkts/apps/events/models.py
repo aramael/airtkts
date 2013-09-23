e6a0bcd873239439e482f5da17597c86afb32c53
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-09-23 13:54:15 -0400

    Create Initial Events Application & Model

---
+++ airtkts/apps/events/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ManyToManyField(User, related_name='owner')
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    location = models.CharField(max_length=100)
