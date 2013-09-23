25d9d5c546040004509d4c2c2380738b181ca22b
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-09-23 15:50:15 -0400

    Added Support for Auto Slugs and Tests

--- airtkts/apps/events/tests.py
+++ airtkts/apps/events/tests.py
@@ -1,16 +1,28 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Event
+from datetime import timedelta
+from django.utils.timezone import now
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class EventTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user('events_test', 'events_test@test.com', 'password')
+
+        # Create 1st Test Event
+        event1 = Event(slug='', name='Event #1', description='some even better event', start_time=now(),
+                       end_time=now() + timedelta(hours=2), location='')
+        event1.save()
+        event1.owner.add(user)
+
+        # Create 2nd Test Event
+        event2 = Event(slug='event-two', name='Event #2', description='some awesome event', start_time=now(),
+                       end_time=now() + timedelta(hours=2), location='')
+        event2.save()
+        event2.owner.add(user)
+
+    def test_event_can_slugify(self):
+        event1 = Event.objects.get(name="Event #1")
+        event2 = Event.objects.get(name="Event #2")
+        self.assertEqual(event1.slug, 'event-1')
+        self.assertEqual(event2.slug, 'event-two')
