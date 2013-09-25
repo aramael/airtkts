ecaea76b1aab797e048c3371bd43f8360d844ad1
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-09-25 15:19:36 -0400

    Added Workflow for Tickets

--- airtkts/apps/events/forms.py
+++ airtkts/apps/events/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-from .models import Event
+from .models import Event, TicketSale
 from airtkts.libs.forms import FieldsetsForm, ActionMethodForm, HideSlugForm
 
 
 class EventForm(ActionMethodForm, HideSlugForm, FieldsetsForm, forms.ModelForm):
 
-    POSSIBLE_ACTIONS = {'_save', '_addanother', '_continue'}
+    POSSIBLE_ACTIONS = {'_save', '_continue'}
 
     fieldsets = (
         (None, {
@@ -25,7 +25,5 @@ class EventForm(ActionMethodForm, HideSlugForm, FieldsetsForm, forms.ModelForm):
     def location_redirect(self, action, instance):
         if action == '_save':
             return {"to": 'event_home'}
-        elif action == '_addanother':
-            return {"to": 'event_new'}
         elif action == '_continue':
-            return {"to": 'event_edit', 'event_id': instance.pk, 'event_slug':instance.slug}
+            return {"to": 'ticketsales_new', 'event_id': instance.pk}
