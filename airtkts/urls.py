e212ef555bfe4f1d2f08e3bf56557ab117f2b005
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-10-03 13:46:09 -0400

    Added User Pages to Forms

--- airtkts/urls.py
+++ airtkts/urls.py
@@ -8,6 +8,13 @@ urlpatterns = patterns('',
     url(r'^$', 'airtkts.views.home', name='home'),
     url(r'^event/(?P<event_id>[0-9]+)/(?P<event_slug>\w+)', 'airtkts.views.ticket_office', name='ticket_office'),
 
+    # User Pages
+    url(r'^accounts/users/$', 'airtkts.views.users_home', name='users_home'),
+    url(r'^accounts/users/new$', 'airtkts.views.users_new', name='users_new'),
+    url(r'^accounts/users/(?P<user_id>[0-9]+)/$', 'airtkts.views.users_edit', name='users_edit'),
+    url(r'^accounts/users/me/$', 'airtkts.views.users_edit', {'self_edit': True}, name='account_edit'),
+
+
     # Account Event Pages
     url(r'^accounts/event/$', 'airtkts.views.event_home', name='event_home'),
     url(r'^accounts/event/new/$', 'airtkts.views.event_form', name='event_new'),
