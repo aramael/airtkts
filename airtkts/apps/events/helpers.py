081974159a64def419004fb89c08b741e2a130dc
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2013-10-06 13:53:13 -0400

    Fixed Error with Accounts Home to Display events that we have access to

--- airtkts/apps/events/helpers.py
+++ airtkts/apps/events/helpers.py
@@ -1,14 +1,6 @@
-from .models import Event
+from guardian.shortcuts import get_objects_for_user
 
 
 def get_events(entity):
 
-    kwargs = {
-        'owner': entity,
-    }
-
-    # Active superusers have all permissions.
-    if entity.is_active and entity.is_superuser:
-        kwargs = {}
-
-    return Event.objects.filter(**kwargs)
+    return get_objects_for_user(entity, 'events.view_event')
